40071c4b658ce5306d6e9927f2647227db6c6f04
AbhimanyuWhitehatjr <96932915+AbhimanyuWhitehatjr@users.noreply.github.com>
2022-01-01 10:27:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDVeQCEyvrln8rvYo_jWIU6eSWBOr8FgNo",
+  authDomain: "bookapp-197d6.firebaseapp.com",
+  projectId: "bookapp-197d6",
+  storageBucket: "bookapp-197d6.appspot.com",
+  messagingSenderId: "624016450883",
+  appId: "1:624016450883:web:9cdd12f3cba11d5a4dbbf6"
+};
